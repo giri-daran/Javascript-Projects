9be346dde9440ed1878f40aa50a2f2f45dd42de3
giri-daran <114486252+giri-daran@users.noreply.github.com>
2022-12-20 15:16:25 +0530

    Add files via upload

---
+++ color-change/colorstyle.js
@@ -0,0 +1,16 @@
+const color=['green', 'orange', 'yellow','blue','red','pink','purple','white','DarkGray'];
+
+const spancolor=document.querySelector('.colorh1');
+const Baner=document.querySelector('.contentOne');
+const btns=document.querySelector('.Btns');
+const header=document.getElementsByTagName('header');
+
+
+btns.addEventListener('click',colorchange);
+
+function colorchange(){
+    const colors=Math.floor(Math.random()*color.length);
+    document.body.style.background=color[colors];
+    spancolor.textContent=color[colors];
+    //console.log(typeof colors);
+}
