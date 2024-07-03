bddae31bdf2997715f785a35713999ff9ea05ddd
giri-daran <114486252+giri-daran@users.noreply.github.com>
2024-07-03 17:16:05 +0530

    Create PrimeNumber.js

---
+++ Leetcode-problem JS/PrimeNumber.js
@@ -0,0 +1,13 @@
+function Primenumber(n){
+    if(n < 2){
+        return `${n} is not a prime number`;
+    }
+    for(let i=2;i<n;i++){
+        if(n%i == 0){
+            return  `${n} is not a prime number`;
+        }
+    }
+    return `${n} is a prime number`;
+}
+
+console.log(Primenumber(6)); //6 is not a prime number
