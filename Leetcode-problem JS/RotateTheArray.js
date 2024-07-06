fd1c2c4e9173d7e9f5cf8027c26241d6bf21b5c8
giri-daran <114486252+giri-daran@users.noreply.github.com>
2024-07-06 13:06:24 +0530

    Create RotateTheArray.js

---
+++ Leetcode-problem JS/RotateTheArray.js
@@ -0,0 +1,18 @@
+//input [1,2,3,4,5,6,7]
+
+//output [5,6,7,1,2,3,4]
+
+function MiddleReverse(input,k){
+    let size = input.length;                    // get the length of the array
+    if(size > k){                               // check condition whethere the array length is higher than given num
+        k=k % size;                             // get reminder as the value of k
+    }
+
+    const rotate = input.splice(size - k , k);  // we put splice so that it removes from (starting , till the end of array)
+    input.unshift(...rotate);                   // now unshift to insert the value in the begining of the array
+    return input;
+}
+
+MiddleReverse([1,2,3,4,5,6,7], 3);
+
+// output [5, 6, 7, 1, 2, 3, 4]
