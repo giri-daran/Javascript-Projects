a30bffd8c228c59545ab234fff95cf375d83de30
giri-daran <114486252+giri-daran@users.noreply.github.com>
2024-07-09 12:54:20 +0530

    Update FabbonacciSeries.js

--- Leetcode-problem JS/FabbonacciSeries.js
+++ Leetcode-problem JS/FabbonacciSeries.js
@@ -4,7 +4,7 @@ function fabbonaci(n){
         //console.log(fib[i]);
         //console.log(fib[i-1]);
         //console.log(fib[i-2]);
-        fib[i] = fib[i-1] + fib[i-2]; //fib[1] + fib[0] = 1  [0,1,1] ; fib[2] + fib[1] [0, 1,1,2] ; fib[3] + fib[2] [0,1,1,2,3]
+        fib[i] = fib[i-1] + fib[i-2]; //fib[1] + fib[0] = 1  [0,1,1] ; fib[2] + fib[1] = 2 [0, 1,1,2] ; fib[3] + fib[2] = 3 [0,1,1,2,3]
         
     }
     return fib //[0,1,1,2,3]
