9a02a8568f8c5d24e60637c47e18206bbed51499
giri-daran <114486252+giri-daran@users.noreply.github.com>
2024-09-09 12:34:43 +0530

    Update SumOfNaturalNumbers.js

--- Leetcode-problem JS/SumOfNaturalNumbers.js
+++ Leetcode-problem JS/SumOfNaturalNumbers.js
@@ -1,3 +1,5 @@
+// Approach 1
+
 function SumOfNaturalNumbers(num){
     let sum = 0;
     for(let i=1 ; i <= num; i++){
@@ -10,3 +12,20 @@ console.log(SumOfNaturalNumbers(5));
 
 //input  = 5
 //output = 15 (1 + 2 + 3 + 4 + 5)
+
+// Approach 2
+
+function SumOfNaturalNumbers(num){
+    
+    return num * (num+1) / 2;
+}
+
+console.log(SumOfNaturalNumbers(5));  
+
+//input  = 5
+//output = 15 (1 + 2 + 3 + 4 + 5)
+
+console.log(SumOfNaturalNumbers(10));  
+
+//input  = 10
+//output = 55 (1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10)
