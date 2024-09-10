497e4b6a41c815a56a41217133b2f51539563493
giri-daran <114486252+giri-daran@users.noreply.github.com>
2024-09-10 18:42:39 +0530

    Update FindValueInArray.js

--- Leetcode-problem JS/FindValueInArray.js
+++ Leetcode-problem JS/FindValueInArray.js
@@ -1,9 +1,8 @@
 //const arr = [1,2,3,4, "hello" , {name : "giri", age : 28}, [4,5,6,7] ]
 
-
 function FindValueInArray(arr, target){
-    for(let x of arr){
-        if(x === target){
+    for(let x of arr){    // for-of gets the value from the array of "arr" places into "x"
+        if(x === target){  
             return true;
         }
     }
